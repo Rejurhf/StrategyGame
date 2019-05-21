50fad7d1ca358a87f88790c0571b6219b8bbcb72
Rejurhf <mziomek108@gmail.com>
2019-05-21 18:54:58 +0200

    Initial commit

---
+++ desktop/src/pl/rejurhf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.rejurhf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.rejurhf.StrategyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StrategyGame(), config);
+	}
+}
