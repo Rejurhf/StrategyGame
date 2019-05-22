5f3573161a9a899429718740331cddc82c0e59e5
Rejurhf <mziomek108@gmail.com>
2019-05-22 09:02:21 +0200

    Board initialization

---
+++ core/src/pl/rejurhf/entities/UnitInstance.java
@@ -0,0 +1,25 @@
+package pl.rejurhf.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class UnitInstance extends Image {
+    private final static int WIDTH = 20;
+    private final static int HEIGHT = 20;
+    private final static String PATHTOUNITS = "units\\";
+
+    private int STARTING_X;
+    private int STARTING_Y;
+
+    public UnitInstance(int x, int y, String texture){
+        super(new Texture(PATHTOUNITS + texture));
+
+        this.setOrigin(WIDTH/2, HEIGHT/2);
+        this.setSize(WIDTH, HEIGHT);
+
+        // Starting position
+        STARTING_X = x;
+        STARTING_Y = y;
+        this.setPosition(STARTING_X, STARTING_Y);
+    }
+}
--- core/src/pl/rejurhf/screens/GameplayScreen.java
+++ core/src/pl/rejurhf/screens/GameplayScreen.java
@@ -3,9 +3,14 @@ package pl.rejurhf.screens;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import pl.rejurhf.StrategyGame;
+import pl.rejurhf.entities.UnitInstance;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameplayScreen extends AbstractScreen {
     private Image bgImg;
+    private List<UnitInstance> unitsList;
 
     public GameplayScreen(StrategyGame game){
         super(game);
@@ -14,6 +19,21 @@ public class GameplayScreen extends AbstractScreen {
     @Override
     protected void init() {
         initBg();
+        initBoard();
+
+    }
+
+    // Board initialization
+    private void initBoard() {
+        unitsList = new ArrayList<UnitInstance>();
+
+        for (int i = 0; i < 70; i++) {
+            for (int j = 0; j < 50; j++) {
+                UnitInstance newUnit = new UnitInstance(i*20, j*20, "empty_unit.png");
+                unitsList.add(newUnit);
+                stage.addActor(unitsList.get(unitsList.size() - 1));
+            }
+        }
     }
 
 
